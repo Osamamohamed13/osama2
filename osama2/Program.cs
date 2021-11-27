33abb2660741247a324c3409b55e7dab893008df
osama eng <osama.eng_5802@eng.kfs.edu.eg>
2021-11-27 12:29:58 +0200

    Add project files.

---
+++ osama2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.Write("Enter the Number to check Prime: ");
+            int n = int.Parse(Console.ReadLine());
+            int m = n / 2;
+            for (int i = 2; i <= m; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write("Number is not Prime.");
+                    count = 1;
+                    break;
+                }
+            }
+            if (count == 0)
+                Console.Write("Number is Prime.");
+
+            Console.ReadKey();
+        }
+    }
+}
